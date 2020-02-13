5ff49a0ef09997ba23639aa786ba1dbcc8566d91
Михаил <shipov-mihail@mail.ru>
2020-02-13 14:32:29 +0300

    WIP: Fixes

---
+++ Controllers/EmployeersController.cs
@@ -0,0 +1,37 @@
+﻿using ASP.NET_Shipov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP.NET_Shipov.Controllers
+{
+    public class EmployeersController : ApiController
+    {
+        Employeers[] employeers = new Employeers[]
+        {
+            new Employeers { ID = 1, Name = "Петр", Salary = 40000, DepName = "Тех" },
+            new Employeers { ID = 2, Name = "Михаил", Salary = 30000, DepName = "Мед"  },
+            new Employeers { ID = 3, Name = "Александр", Salary = 50000, DepName = "Авто"},
+            new Employeers { ID = 4, Name = "Николай", Salary = 45000, DepName = "Тех" },
+            new Employeers { ID = 5, Name = "Анастасия", Salary = 38000, DepName = "Мед" },
+            new Employeers { ID = 6, Name = "Елена", Salary = 43000, DepName = "Авто" },
+        };
+        public IEnumerable<Employeers> GetAllProducts()
+        {
+            return employeers;
+        }
+        public IHttpActionResult GetProduct(int id)
+        {
+            var employee = employeers.FirstOrDefault((emp) => emp.ID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+    }
+}
---
+++ Models/Employeers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Shipov.Models
+{
+    public class Employeers
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string DepName { get; set; }
+    }
+}
